cbba805dd93d5a7394f650d62dfe413b0f1e89a4
phiresky <phireskyde+git@gmail.com>
2015-11-29 05:22:42 +0100

    set autocomp on load

--- src/render.ts
+++ src/render.ts
@@ -18,11 +18,17 @@ function updateQueryString() {
 }
 declare var dcodeIO:any, $:any, AutoComplete:any;
 (<any>window).ProtoBuf = dcodeIO.ProtoBuf;
+type IndexedSeries = [number, imdbproto.DB.Series];
+
 let renderSuccess = false;
-let database : imdbproto.DB;
-function getSeries(title: string, year: number) {
+let database: imdbproto.DB;
+let currentDisplay: IndexedSeries[] = [];
+let autoComplete: any;
+function getSeries(title: string, year: number):[number, imdbproto.DB.Series] {
+	let i = 0;
 	for(const series of database.series) {
-		if(series.title === title && series.year === year) return series;
+		if(series.title === title && series.year === year) return [i,series];
+		i++;
 	}
 }
 function episodeString(ep:imdbproto.DB.Series.Episode) {
@@ -42,13 +48,15 @@ function tryShowInitialChart() {
 	});
 	if(series.every(s => s !== undefined)) {
 		renderSuccess = true;
+		autoComplete.setValue(series.map(([i,s]) => [seriesToAutocomplete(s,i)]));
 		showChart(series);
 	}
 }
-function showChart(series: imdbproto.DB.Series[]) {
+function showChart(series: IndexedSeries[]) {
+	currentDisplay = series;
 	if(series.length === 0) return;
-	const title = series.map(s => s.title).join(", ");
-	let allEps:imdbproto.DB.Series.Episode[] = [].concat.apply([], series.map(s => s.episodes));
+	const title = series.map(s => s[1].title).join(", ");
+	let allEps:imdbproto.DB.Series.Episode[] = [].concat.apply([], series.map(s => s[1].episodes));
 	allEps.sort((a,b) => a.season - b.season || a.episode - b.episode);
 	allEps = allEps.filter((ep, i) => i == 0 || ep.season != allEps[i-1].season || ep.episode != allEps[i-1].episode);
 	const seasons:{from:number, to: number}[] = [{from:0, to:0}];
@@ -87,7 +95,7 @@ function showChart(series: imdbproto.DB.Series[]) {
 		tooltip: {
 			formatter: function() {return `${this.series.name} <em>${episodeString(this.point.episode)}</em>`}
 		},
-		series: series.map(s => ({
+		series: series.map(([i,s]) => ({
 			name: s.title,
 			//lineWidth: 0,
 			marker: {
@@ -96,34 +104,35 @@ function showChart(series: imdbproto.DB.Series[]) {
 			data: s.episodes.map((episode,i) => ({name:episodeString(episode), x:episode.episode + seasons[episode.season].from, y:episode.rating/10, episode}))
 		}))
 	});
-	qd["t"] = series.map(s => `${s.title} ${s.year}`.replace(/ /g, "_")).join(" ");
+	qd["t"] = series.map(([i,s]) => `${s.title} ${s.year}`.replace(/ /g, "_")).join(" ");
 	updateQueryString();
 }
 function seriesToAutocomplete(series: imdbproto.DB.Series, inx: number) {
-	return {optionHTML:`${series.title} (${series.year})`, value:inx};
+	const txt = `${series.title} (${series.year})`;
+	return {optionHTML:txt, tokenHTML:txt, value:inx};
 }
 declare var _schema: any; // added by makefile
 {
 	const builder:imdbproto.ProtoBufBuilder = ProtoBuf.loadJson(_schema).build("imdbproto") as any;
 	getArrayBuffer("basedata-popular.buf.js", baseData => {
 		database = builder.DB.decode(baseData);
-		const comp = new AutoComplete("search", {
+		autoComplete = new AutoComplete("search", {
 			placeholderHTML: "Search for TV series...",
 			
 			lists: {series: {
 				options:database.series.map(seriesToAutocomplete),
 				//optionHTML: (s:imdbproto.DB.Series) => s.title,
 				//tokenHTML: x => "a"
-				matchOptions: (inp:string, res:any[]) => res.slice(0, 20),
+				matchOptions: (inp:string, res:any[]) => res.slice(0, 20).filter(v => !currentDisplay.some(([i,s])=>i===v.value)),
 			}},
-			onChange: (val:any[]) => showChart(val.map(v => database.series[v[0].value]))
+			onChange: (val:any[]) => showChart(val.map(v => [v[0].value,database.series[v[0].value]] as IndexedSeries))
 		});
 		if(qd["t"]) tryShowInitialChart();
 		getArrayBuffer("basedata-unpopular.buf.js", baseData2 => {
 			const db2 = builder.DB.decode(baseData2);
 			for(const series of db2.series) {
 				const inx = database.series.push(series) - 1;
-				comp.addOption("series", seriesToAutocomplete(series, inx));
+				autoComplete.addOption("series", seriesToAutocomplete(series, inx));
 			}
 			if(qd["t"] && !renderSuccess) tryShowInitialChart();
 		});
